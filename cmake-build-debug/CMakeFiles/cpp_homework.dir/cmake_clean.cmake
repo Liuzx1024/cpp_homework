d82b724eac2a177eda03ee743dec66936efba09e
liuzx1024 <1227491292@qq.com>
2021-06-07 16:08:33 +0800

    update 06,07,2021

---
+++ cmake-build-debug/CMakeFiles/cpp_homework.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_homework.dir/subclass.cpp.o"
+  "cpp_homework"
+  "cpp_homework.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_homework.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
